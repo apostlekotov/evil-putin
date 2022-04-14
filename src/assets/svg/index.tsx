4a3b1fe58f21173aff380fc2884f572b3be9f050
Kotov <kotov.smtp@gmail.com>
2022-04-14 03:18:19 +0300

    🖼 svg assets added

---
+++ src/assets/svg/index.tsx
@@ -0,0 +1,15 @@
+import LogoIcon from "@/public/svg/logo.svg";
+import CircleIcon from "@/public/svg/circle.svg";
+
+// Socials
+import DiscordIcon from "@/public/svg/discord.svg";
+import InstagramIcon from "@/public/svg/instagram.svg";
+import TwitterIcon from "@/public/svg/twitter.svg";
+
+export const socialIcons: { [x: string]: JSX.Element } = {
+  discord: <DiscordIcon />,
+  instagram: <InstagramIcon />,
+  twitter: <TwitterIcon />,
+};
+
+export { LogoIcon, CircleIcon };
