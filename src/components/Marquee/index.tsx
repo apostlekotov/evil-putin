a954b78e61c021d6be5ad5efbb8a5fcffd673914
Kotov <kotov.smtp@gmail.com>
2022-04-14 22:48:57 +0300

    ⚛️ marquee component added

--- src/components/Marquee/index.tsx
+++ src/components/Marquee/index.tsx
@@ -1,3 +1,10 @@
-export const Marquee: React.FC = () => (
-  <strong className='uppercase'>Stop russian aggression</strong>
+import FastMarquee from "react-fast-marquee";
+
+export const Marquee: React.FC = ({ children }) => (
+  <FastMarquee
+    gradient={false}
+    className='bg-[#BEFD37] before:hidden after:hidden overflow-hidden text-black font-medium text-2xl md:text-5xl space-x-4 p-3 uppercase'
+  >
+    {children} {children} {children} {children} {children} {children}
+  </FastMarquee>
 );
