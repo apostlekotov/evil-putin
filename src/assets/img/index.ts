3ef3713cece7a658b8c9ea808542e39b6516163a
Kotov <kotov.smtp@gmail.com>
2022-04-14 22:49:06 +0300

    🖼 assets added

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,3 +1,6 @@
 import LogoImage from "@/public/img/logo.png";
+import HeroImage from "@/public/img/hero.png";
+import BombImage from "@/public/img/bomb.png";
+import HandImage from "@/public/img/hand.png";
 
-export { LogoImage };
+export { LogoImage, HeroImage, BombImage, HandImage };
