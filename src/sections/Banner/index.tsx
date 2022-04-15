89598ac8e1a5a23ac57ee2222834ab51a4e259fb
Kotov <kotov.smtp@gmail.com>
2022-04-15 18:16:12 +0300

    ⚛️ banner added

--- src/sections/Banner/index.tsx
+++ src/sections/Banner/index.tsx
@@ -1,1 +1,16 @@
-export const Banner: React.FC = () => <></>;
+export const Banner: React.FC = () => (
+  <section className='banner'>
+    <span className="before:content-['UKRAINE!_'] after:content-['WAR_IN']">
+      STOP
+    </span>
+    <span className="before:content-['STOP_'] after:content-['IN_UKRAINE!']">
+      WAR
+    </span>
+    <span className="before:content-['STOP_WAR_'] after:content-['UKRAINE!']">
+      IN
+    </span>
+    <span className="before:content-['WAR_IN_'] after:content-['STOP']">
+      UKRAINE!
+    </span>
+  </section>
+);
