610ddaea27dc70497beacf5429ead5f0ce0c5721
Kotov <kotov.smtp@gmail.com>
2022-04-15 18:15:54 +0300

    ⚛️ speed fix

--- src/components/Marquee/index.tsx
+++ src/components/Marquee/index.tsx
@@ -3,6 +3,7 @@ import FastMarquee from "react-fast-marquee";
 export const Marquee: React.FC = ({ children }) => (
   <FastMarquee
     gradient={false}
+    speed={50}
     className='bg-[#BEFD37] before:hidden after:hidden overflow-hidden text-black font-medium text-2xl md:text-5xl space-x-4 p-3 uppercase'
   >
     {children} {children} {children} {children} {children} {children}
