532869e8b98af03a4a02bd0ca85561625a4fc6fd
Kotov <kotov.smtp@gmail.com>
2022-04-16 23:41:31 +0300

    ⚛️ metaverse and about sections added

--- src/sections/AboutUs/index.tsx
+++ src/sections/AboutUs/index.tsx
@@ -1,1 +1,44 @@
-export const AboutUs: React.FC = () => <></>;
+import Image from "next/image";
+import { HornyHuiloImage, SadHuiloImage } from "@/assets/img";
+
+export const AboutUs: React.FC = () => (
+  <section className='flex flex-col relative items-center min-h-screen justify-center overflow-hidden'>
+    <div className='md:absolute -rotate-90 md:rotate-0 max-w-sm md:max-w-none md:w-[40rem] lg:w-[56rem] right-0 md:right-auto md:left-0 md:-translate-x-[45%] top-0 lg:top-[-10%] self-end -z-10'>
+      <Image
+        src={HornyHuiloImage}
+        objectFit='contain'
+        alt='horny huilo'
+        placeholder='blur'
+      />
+    </div>
+
+    <div className='flex flex-col px-8 max-w-2xl xl:max-w-3xl mx-auto -mb-20 -mt-16 md:my-auto'>
+      <h2 className='uppercase text-5xl md:text-7xl text-center md:text-left font-medium mb-8 md:mb-16'>
+        About Us
+      </h2>
+
+      <p className='text-base md:text-lg mb-4'>
+        Make Art Not War (MANW) was created to help the people of Ukraine. We
+        aim to use the power of crypto and the communities within it to raise
+        funds for Ukraine. We strive for peace in Ukraine and for a life without
+        fear in our country. With every MANW NFT transaction, you will directly
+        support Ukraine and its people which will directly receive 60% of the
+        minting fee. This will happen automatically through our contract and
+        will immediately be redirected to Ukraine’s official donation wallet.
+      </p>
+      <p className='text-base md:text-lg mb-6'>
+        By minting our project, you donate money – and in exchange receive a
+        valuable NFT that you can resell.
+      </p>
+    </div>
+
+    <div className='md:absolute rotate-90 md:rotate-0 max-w-sm md:max-w-none md:w-[40rem] lg:w-[56rem] left-0 md:left-auto md:right-0 md:translate-x-[45%] bottom-0 md:bottom-auto lg:top-[-10%] self-start -z-10'>
+      <Image
+        src={SadHuiloImage}
+        objectFit='contain'
+        alt='sad huilo'
+        placeholder='blur'
+      />
+    </div>
+  </section>
+);
--- src/sections/Metaverse/index.tsx
+++ src/sections/Metaverse/index.tsx
@@ -1,1 +1,55 @@
-export const Metaverse: React.FC = () => <></>;
+import Image from "next/image";
+import { NukeLink } from "@/components/NukeLink";
+import { Matr1Image, Matr2Image, Matr3Image } from "@/assets/img";
+
+export const Metaverse: React.FC = () => (
+  <section className='flex flex-col-reverse relative lg:flex-row items-center lg:items-end py-20 overflow-hidden'>
+    <div className='absolute w-[12rem] md:w-[24vw] 2xl:w-auto 2xl:scale-[.80] rotate-[30deg] md:rotate-[-30deg] right-0 2xl:right-[3vw] top-0 lg:-top-1/4 opacity-[0.35] -z-10'>
+      <Image
+        src={Matr3Image}
+        objectFit='contain'
+        alt='matr third'
+        placeholder='blur'
+      />
+    </div>
+    <div className='absolute w-[16rem] sm:w-[22rem] lg:w-[34vw] 2xl:w-auto scale-x-[-1] lg:scale-x-100 scale-100 2xl:scale-90 -left-[10%] md:left-0 lg:left-[40%] rotate-[-16deg] lg:rotate-[16deg] bottom-1/4 lg:bottom-auto lg:-top-1/4 -z-10'>
+      <Image
+        src={Matr2Image}
+        objectFit='contain'
+        alt='matr second'
+        placeholder='blur'
+      />
+    </div>
+    <div className='pr-24 pl-4 pt-12 lg:px-4 -mb-2 lg:pr-0 lg:ml-20 max-w-md lg:max-w-xl relative scale-x-[-1] lg:scale-x-100'>
+      <Image
+        src={Matr1Image}
+        objectFit='contain'
+        alt='matr first'
+        placeholder='blur'
+      />
+    </div>
+
+    <div className='flex flex-col px-8 max-w-xl mx-auto'>
+      <h3 className='uppercase text-4xl md:text-7xl text-center md:text-left font-medium mb-8 md:mb-16'>
+        Metaverse
+      </h3>
+
+      <p className='text-base md:text-lg mb-4'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut diam
+        dictum morbi. Sit justo erat sit sit enim elit. Egestas fringilla
+        dignissim congue ridiculus aenean sem sodales semper. Mattis risus,
+        nunc, cursus lectus est et. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Aliquam ut diam dictum morbi. Sit justo erat sit sit
+        enim elit.
+      </p>
+      <p className='text-base md:text-lg mb-6'>
+        Egestas fringilla dignissim congue ridiculus aenean sem sodales semper.
+        Mattis risus, nunc, cursus lectus est et..
+      </p>
+
+      <NukeLink className='flex self-start -ml-10' href='/'>
+        More Info
+      </NukeLink>
+    </div>
+  </section>
+);
