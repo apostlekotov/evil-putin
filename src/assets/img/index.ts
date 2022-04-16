599f119f9cb81e5ef294b8580312436dfa2e7683
Kotov <kotov.smtp@gmail.com>
2022-04-16 23:40:24 +0300

    🖼 assets added

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -2,6 +2,22 @@ import LogoImage from "@/public/img/logo.png";
 import HeroImage from "@/public/img/hero.png";
 import NukeImage from "@/public/img/nuke.png";
 import HandImage from "@/public/img/hand.png";
+import SadHuiloImage from "@/public/img/sad-huilo.png";
+import HornyHuiloImage from "@/public/img/horny-huilo.png";
+import Matr1Image from "@/public/img/matr1.png";
+import Matr2Image from "@/public/img/matr2.png";
+import Matr3Image from "@/public/img/matr3.png";
 import FooterBGImage from "@/public/img/footer-bg.png";
 
-export { LogoImage, HeroImage, NukeImage, HandImage, FooterBGImage };
+export {
+  LogoImage,
+  HeroImage,
+  NukeImage,
+  HandImage,
+  SadHuiloImage,
+  HornyHuiloImage,
+  Matr1Image,
+  Matr2Image,
+  Matr3Image,
+  FooterBGImage,
+};
