dd40c8af965f32eb26236e8c66c26dea2c9e794f
Kotov <kotov.smtp@gmail.com>
2022-06-06 19:19:03 +0300

    ⚛️ questions added

--- src/data/questions.tsx
+++ src/data/questions.tsx
@@ -2,28 +2,29 @@ import { Question } from "types";
 
 export const questions: Question[] = [
   {
-    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    question: "What is EP?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat quis suspendisse massa vulputate massa eget praesent molestie arcu. Eu suscipit orci diam ut tellus pellentesque sed erat ultricies. Bibendum sodales dignissim aliquet felis, scelerisque ornare.",
+      "EvilPutin is a community-driven NFT project that strives to utilize the WEB 3.0 space to help people in need with the knowledge of the creators and the community's voice working in tandem. In the later stages, we want to transform the project into an NFT-gaming project, where the community can do more than just talking in discord. We are forever versatile and strongly believe in purpose.",
   },
   {
-    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    question: "Who created EP?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat quis suspendisse massa vulputate massa eget praesent molestie arcu. Eu suscipit orci diam ut tellus pellentesque sed erat ultricies. Bibendum sodales dignissim aliquet felis, scelerisque ornare.",
+      "EvilPutin was created by Web 3.0 enthusiasts and professionals that lived in Ukraine before the War. They had different projects, but as the War began, they dropped everything and decided to create a new project that would help the people that are around them.",
   },
   {
-    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat quis suspendisse massa vulputate massa eget praesent molestie arcu. Eu suscipit orci diam ut tellus pellentesque sed erat ultricies. Bibendum sodales dignissim aliquet felis, scelerisque ornare.",
+    question: "Who did the art for EP?",
+    answer: "The art was done by a design studio in Ukraine: ___________",
   },
   {
-    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat quis suspendisse massa vulputate massa eget praesent molestie arcu. Eu suscipit orci diam ut tellus pellentesque sed erat ultricies. Bibendum sodales dignissim aliquet felis, scelerisque ornare.",
+    question: "When is The EP Mint?",
+    answer: "The EP mint date is still TBA.",
   },
   {
-    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat quis suspendisse massa vulputate massa eget praesent molestie arcu. Eu suscipit orci diam ut tellus pellentesque sed erat ultricies. Bibendum sodales dignissim aliquet felis, scelerisque ornare.",
+    question: "What is The Total Mint Supply?",
+    answer: "The EP collection will consist of 7'777 unique NFTs.",
+  },
+  {
+    question: "What is The Mint Price?",
+    answer: "The EP mint price is still TBA.",
   },
 ];
